0ad29228318a3afc5690a4aa3d6f9cde773af097
Dr. Jenkem <drjenkem@protonmail.com>
2020-05-13 10:11:35 -0700

    z_kanfont: func_8006EEBC is now matching

--- src/code/z_kanfont.c
+++ src/code/z_kanfont.c
@@ -3,6 +3,9 @@
 
 #pragma GLOBAL_ASM("asm/non_matchings/code/z_kanfont/func_8006EE60.s")
 
-#pragma GLOBAL_ASM("asm/non_matchings/code/z_kanfont/func_8006EEBC.s")
+void func_8006EEBC(s32 arg0, u16 arg1) {
+	u32 vrom = (u32)&_message_staticSegmentRomStart + ((arg1 << 7) + 0x4000);
+    DmaMgr_SendRequest1(arg0 + 0x3C08, vrom, 0x80, "../z_kanfont.c", 100);
+}
 
 #pragma GLOBAL_ASM("asm/non_matchings/code/z_kanfont/func_8006EF10.s")
